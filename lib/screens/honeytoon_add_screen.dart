8984a2c602edaba7297ae66cd1ea6d320f8de79f
yoojh9 <yoojh9@gmail.com>
2020-07-14 17:28:22 +0900

    작품 추가 스크린 add

--- lib/screens/honeytoon_add_screen.dart
+++ lib/screens/honeytoon_add_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class HoneytoonAddScreen extends StatelessWidget {
+class HoneytoonAddScreen extends StatefulWidget {
+
+  @override
+  _HoneytoonAddScreenState createState() => _HoneytoonAddScreenState();
+}
+
+class _HoneytoonAddScreenState extends State<HoneytoonAddScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('허니툰 추가'),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '작품 제목'
+                )
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '작품 설명'
+                )
+              ),
+            ]
+          ),
+        ),
+      ) 
+        
     );
   }
 }
